dd22dbe0c28a30f31b2ff754e1c86d310e2d2944
theYahy0 <oktamjanovyahyo11@gmail.com>
2025-02-26 03:51:42 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+// import Scientific from './sciencePage/scientificjournals/Scientific';
+import ScienceCenter from './sciencePage/sciencecenter/ScienceCenter';
+
+// import Academy from './sciencePage/academiccouncil/Academy';
+
+function App() {
+  
+
+  return (
+    <div>
+      {/* <Academy /> */}
+      <ScienceCenter />
+      {/* <Scientific /> */}
+    </div>
+  )
+}
+
+export default App;
---
+++ src/sciencePage/Science.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Academy from './academiccouncil/Academy'
+import ScienceCenter from './sciencecenter/ScienceCenter'
+import Scientific from './scientificjournals/Scientific'
+export const Science = () => {
+    return (
+        <div>
+
+            <Academy />
+            <ScienceCenter />
+            <Scientific />
+
+        </div>
+    )
+}
